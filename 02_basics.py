31f919e756f7920075fb51cbe2ff3b7ce887c0a6
LlamaUnicorn <holidaynight@gmail.com>
2024-04-27 06:53:17 +0300

    27.04.2024 docker exec, logs

--- 02_basics.py
+++ 02_basics.py
@@ -4,10 +4,10 @@
 # docker run --name my_first_container busybox:latest
 
 # docker run --detach --interactive --tty --name alpine alpine:latest
-# --detach | -d - run in background mode
-# ??? --interactive | -i - provide input/output
-# ??? --tty | -t - provide terminal for interactive mode
 # shortened: docker run -dit --name alpine alpine:latest
+# --detach | -d - run in background mode
+# --interactive | -i - provide input/output
+# --tty | -t - provide terminal for interactive mode
 
 # Run container once and print ping output in terminal
 # docker run -it --name my_container1 busybox:latest ping -c 6 localhost
@@ -30,3 +30,24 @@
 
 # Create but don't run a container
 # docker container create --name my_container2 alpine:latest
+
+
+# SH into a running container
+# docker run -dit --name test_cont busybox:latest
+# docker exec -i -t test_cont sh
+
+
+# Inspect containers
+# docker container inspect test_cont
+
+# Logs
+# Check logging driver
+# docker info --format '{{.LoggingDriver}}'
+
+# Change logging driver: run container with --log-driver or --log-opt
+
+# docker run --name test -dit alpine:latest sh -c "while true; do $(echo time) sleep 10; done"
+# cd /var/lib/docker/containers
+# ls
+# cd ls -ltr
+# cat -json.log
